717223e494b6116b29887056f27fafb0d69ea39e
Roshan Bichve <roshanbichve8866@gmail.com>
2022-06-06 01:12:24 +0530

    27-05-2022

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Col, Container, Row } from "react-bootstrap";
+import Logo from "../images/logo.svg";
 
 const Header = () => {
     return (
@@ -7,14 +8,32 @@ const Header = () => {
                 <Container fluid>
                     <Row>
                         <Col lg={3}>
-                            <h1>Header</h1>
+                            <a href="/" className="site_logo" title="Logo">
+                                <img src={Logo} alt="Qr_Logo" />
+                            </a>
                         </Col>
                         <Col lg={6}>
-                            <h1>Header</h1>
+                            <nav className="header_menu">
+                                <ul>
+                                    <li>
+                                        <a href="#" title="Home">
+                                            Home
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" title="Faq">
+                                            Faq
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" title="Contact">
+                                            Contact
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
                         </Col>
-                        <Col lg={3}>
-                            <h1>Header</h1>
-                        </Col>
+                        <Col lg={3}></Col>
                     </Row>
                 </Container>
             </header>
